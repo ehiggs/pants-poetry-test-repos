eed7bfcdf2a16bb419a0103ed30a6b2c5bfd787a
Ewan Higgs <ewan.higgs@gmail.com>
2021-07-31 23:09:57 +0200

    Add fastapi project which depends on the same libs as the flask one.

--- multirepo-1/project-app-async/app.py
+++ multirepo-1/project-app-async/app.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
-from fastapi import Flask
+from fastapi import FastAPI
 import uvicorn
 from project_models.models import Greeting
 
+# Because python is interpreted and each package is built from the same top 
+# level venv, we can do things we wouldn't really want, like import packages 
+# we don't depend on in this subproject.
+from flask import Flask
+
 app = FastAPI()
 
-@app.get("/hello/<name>")
-def hello(name):
+
+@app.get("/")
+async def root():
+    return f"Hello, try using /hello/<name>"
+
+@app.get("/hello/{name}")
+async def hello(name):
     greeting = Greeting(greet='hello', name=name)
     return f"{greeting.greet}, {greeting.name}"
 
